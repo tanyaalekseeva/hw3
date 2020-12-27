add1d5ad39b6d7d29ecf9164138ac1797e17dab3
tanyaalekseeva <74408535+tanyaalekseeva@users.noreply.github.com>
2020-12-27 21:12:07 +0300

    Update homework2.cpp

--- homework2.cpp
+++ homework2.cpp
@@ -1,4 +1,5 @@
  #include <cstddef> 
+ #include <iostream>
 
 using namespace std; 
 
@@ -22,7 +23,7 @@ class Deque
 	Node* rear; 
 	Node* size; 
 
-public: 
+private: 
 	Deque() 
 	{ 
 		front = rear = NULL; 
